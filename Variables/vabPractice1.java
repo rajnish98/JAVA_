af745faca7826bc982640736875da0287dc37f5d
rajnish98 <rajnishpandey63@gmail.com>
2025-08-20 09:47:57 +0530

    variables Prcatice

---
+++ Variables/vabPractice1.java
@@ -0,0 +1,17 @@
+public class vabPractice1 {
+  public static void main(String[] args) {
+    int roll_no = 5;
+    double height = 5.9;
+    char gender = 'M';
+    boolean isEmployed = true;
+
+    // using string
+    String FullName = "Rajnish Pandey";
+
+    System.out.println("Roll no: " + roll_no);
+    System.out.println("Height: " + height);
+    System.out.println("Gender: " + gender);
+    System.out.println("Is Employed: " + isEmployed);
+    System.out.println("Full Name: " + FullName);
+  }
+}
