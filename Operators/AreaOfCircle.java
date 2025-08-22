1c6dbe73ba43952d48ad0cb097bb58e424d30a06
rajnish98 <rajnishpandey63@gmail.com>
2025-08-22 09:36:02 +0530

    variables

---
+++ Operators/AreaOfCircle.java
@@ -0,0 +1,11 @@
+public class AreaOfCircle {
+  public static void main(String[] args) {
+    double radius = 6.0;
+    double AreaOfCircle = Math.PI * radius * radius;
+    System.out.println("Area of circle: " + AreaOfCircle);
+    double Perimeter = 2 * Math.PI * radius;
+    System.out.println("perimeter of a circle: " + Perimeter);
+  }
+
+  
+  }
