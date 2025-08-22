f1c14a5a0677542fc96e28309dae3d74aa1e01a9
rajnish98 <rajnishpandey63@gmail.com>
2025-08-22 08:06:01 +0530

    variables Prcatice

---
+++ Variables/varPractice2.java
@@ -0,0 +1,15 @@
+public class varPractice2 {
+  public static void main(String[] args) {
+    double Celcius = 10.5;
+    double F = (Celcius * 9/5 + 32);
+
+    System.out.println("Fahrenheit is " + F);
+
+    // type cast
+    int Fahrenheit = (int) F;
+    System.out.println("Final output " + Fahrenheit);
+
+
+
+  }
+}
