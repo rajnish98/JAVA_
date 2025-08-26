9ffd5f7c2bf6465220558b7afe61a0fde6469039
rajnish98 <rajnishpandey63@gmail.com>
2025-08-26 06:02:53 +0530

    identifire and keyword

---
+++ Identifire_Keyword/Idekey.java
@@ -0,0 +1,11 @@
+public class Idekey{
+  public static void main(String[] args){
+    final int x = 10;
+
+    if(x > 10){
+      System.out.println("Failed");
+    }else{
+      System.out.println("Success");
+    }
+  }
+}
---
+++ InputUser/ScannerPractice1.java
@@ -0,0 +1,5 @@
+public class ScannerPractice1 {
+  public static void main(String[] args) {
+    
+  }
+}
