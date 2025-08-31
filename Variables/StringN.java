77db65e05dfcfb2cd5708ed3b94e40cbee7eefc6
rajnish98 <rajnishpandey63@gmail.com>
2025-08-31 15:38:04 +0530

    string types

---
+++ Variables/StringN.java
@@ -0,0 +1,11 @@
+public class StringN {
+  // Demonstrating String data type
+    public static void main(String[] args) {
+        String n = "Geek1";
+        String m = "Hello, World!";
+
+        System.out.println("Name: " + n); 
+        System.out.println("Message: " + m); 
+    }
+}
+
